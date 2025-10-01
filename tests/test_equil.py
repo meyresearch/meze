0d563a45b4401dc6f22dfa22b5b46cce2d7944d4
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2025-10-01 18:18:07 +0100

    tests: add test for cold meze construction

---
+++ tests/test_equil.py
@@ -0,0 +1,10 @@
+from meze import ColdMeze
+
+project_dir = "/Users/af25016/projects/indole-carboxylates/vim2/system_preparation/water"
+cold_meze = ColdMeze.from_files(topology=f"{project_dir}/vim2_solv.prmtop",
+                                coordinates=f"{project_dir}/vim2_solv.inpcrd",
+                                group_name="vim2_wat")
+
+
+cold_meze.minimise()
+
