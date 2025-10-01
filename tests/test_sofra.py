0e45ce3635369b64c18fd0acc09b982828d890cc
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2025-10-01 17:02:09 +0100

    wip: test creating meze and getting active site

--- tests/test_sofra.py
+++ tests/test_sofra.py
@@ -1,8 +1,11 @@
 from meze import Sofra, Meze
 
 project_dir = "/Users/af25016/projects/indole-carboxylates/vim2/system_preparation/water"
-sofra = Sofra(protein_file=f"{project_dir}/vim2_solv.pdb")
-print(sofra)
-
-meze_ = Meze(protein_file=f"{project_dir}/vim2_solv.pdb")
+meze_ = Meze.from_files(topology=f"{project_dir}/vim2_solv.prmtop",
+                        coordinates=f"{project_dir}/vim2_solv.inpcrd",
+                        group_name="vim2_wat")
 print(meze_)
+
+active_site = meze_.get_active_site()
+for atom in active_site:
+    print(atom.resname, atom.resid)
