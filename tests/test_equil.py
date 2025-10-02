f839d4078f7b6611cc6cfa14b91f3e46424bc9a7
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2025-10-02 11:32:55 +0100

    test: create equil directory

--- tests/test_equil.py
+++ tests/test_equil.py
@@ -1,11 +1,15 @@
 from meze import ColdMeze
+import os
 
-project_dir = "/Users/af25016/projects/indole-carboxylates/vim2/system_preparation/water"
-cold_meze = ColdMeze.from_files(topology=f"{project_dir}/vim2_solv.prmtop",
-                                coordinates=f"{project_dir}/vim2_solv.inpcrd",
+project_dir = "/Users/af25016/projects/indole-carboxylates/vim2/"
+input_dir = f"{project_dir}/system_preparation/water"
+cold_meze = ColdMeze.from_files(topology=f"{input_dir}/vim2_solv.prmtop",
+                                coordinates=f"{input_dir}/vim2_solv.inpcrd",
                                 group_name="vim2_wat")
 
+equil_dir = os.path.join(project_dir, "equilibration")
+os.makedirs(equil_dir, exist_ok=True)
 
 cold_meze.minimise(position_restraints="all",
-                   workdir=project_dir)
+                   workdir=equil_dir)
 
