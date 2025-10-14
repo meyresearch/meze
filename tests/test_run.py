39259def53422d7c27ea44d7884032130f334786
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2025-10-14 10:37:10 +0100

    tests: test production setup

---
+++ tests/test_run.py
@@ -0,0 +1,22 @@
+from meze import HotMeze
+import os
+
+project_dir = "/Users/af25016/projects/indole-carboxylates/vim2/"
+equilibration_dir = f"{project_dir}/system_preparation/water"
+
+meze = HotMeze.from_files(
+    topology=f"{equilibration_dir}/next.prm7",
+    coordinates=f"{equilibration_dir}/next.rst7",
+    group_name="vim2_wat",
+    path_to_engine=os.path.join(os.environ["AMBERHOME"], "bin", "sander")
+)
+
+outputs_dir = os.path.join(project_dir, "outputs", "water")
+os.makedirs(outputs_dir, exist_ok=True)
+
+
+meze.run(
+    process_name="test_prod",
+    workdir=outputs_dir,
+    runtime=0.5
+)
