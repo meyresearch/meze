f4d554988d2743376d0089c32f3792ca6133ddf0
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2025-10-15 18:04:07 +0100

    wip: test for dis res

---
+++ tests/test_distance_restraints.py
@@ -0,0 +1,31 @@
+from meze import HotQuantumMeze
+import os
+
+system_name = "l1"
+bridging_water = "water"
+
+topology_filename = "L153_1SML_HOH_wat.parm7"
+coordinate_filename = "L153_1SML_HOH_wat.rst7"
+
+project_dir = f"/Users/af25016/projects/indole-carboxylates/{system_name}/"
+input_dir = f"{project_dir}/system_preparation/{bridging_water}"
+
+hot_qm_meze = HotQuantumMeze.from_files(
+    topology=f"{input_dir}/{topology_filename}",
+    coordinates=f"{input_dir}/{coordinate_filename}",
+    group_name=f"{system_name}_{bridging_water}",
+    path_to_executable=os.path.join(
+        os.environ["AMBERHOME"], "bin", "sander"
+    )
+)
+
+qmmm_dir = os.path.join(project_dir, "qmmm", bridging_water)
+os.makedirs(qmmm_dir, exist_ok=True)
+
+
+hot_qm_meze.run(
+    workdir=qmmm_dir,
+    process_name="03_qmmm_prod",
+    runtime=5,
+    metal_resids_for_distance_restraints=[536, 537]
+)
