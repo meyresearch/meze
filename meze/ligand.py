606a7f4657c978e140af802fceced6ddbad52588
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2026-01-06 15:07:55 +0200

    feat: complex prep for mcpb
    rebase #5

--- meze/ligand.py
+++ meze/ligand.py
@@ -66,6 +66,7 @@ class Ligand():
             lines = ifile.readlines()
         old_resname = [line.split()[3] for line in lines][0]
         new_lines = [line.replace(old_resname, residue_name) for line in lines]
+        
         with open(f"{path}/{residue_name}.pdb", "w") as ofile:
             ofile.writelines(new_lines)
 
