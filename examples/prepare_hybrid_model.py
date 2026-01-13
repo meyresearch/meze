848126c1d3ddac4c4f00b502b0317a27a74af00d
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2026-01-13 15:22:14 +0000

    refactor: add correct path to examples
    fixing
    
    rebase #11

--- examples/prepare_hybrid_model.py
+++ examples/prepare_hybrid_model.py
@@ -9,7 +9,7 @@ system_name = "vim2"
 ligand_name = sys.argv[2]
 
 # set ColdMezeRecipe including model (i.e. metal params), ligand(?)
-with open(f"{project_dir}/inputs/hybrid_model/model_ezaff_recipe.json", "r") as file:
+with open(f"{project_dir}/inputs/hybrid_model/protein/{system_name}/model_ezaff_recipe.json", "r") as file:
     json_recipe = json.load(file)
 
 # json_recipe["path_to_engine"] = os.path.join(
@@ -22,45 +22,37 @@ cold_meze = ColdMeze.from_files(
 )
 
 cold_meze_with_lig = cold_meze.add_ligand(
-    ligand_file=f"{project_dir}/inputs/hybrid_model/ligands/{ligand_name}.pdb",
+    ligand_file=f"{project_dir}/inputs/hybrid_model/ligands/{system_name}/{ligand_name}.pdb",
     ligand_charge=-1,
     name="MOL"
-
+)
 #TODO make non standard res a union of Ligand and List[Ligand]
 cold_system = cold_meze_with_lig.add_non_standard_residue(
-    file=f"{project_dir}/inputs/hybrid_model/protein/MOH.pdb",
+    file=f"{project_dir}/inputs/hybrid_model/protein/{system_name}/MOH.pdb",
     charge=-1,
     atom_type="amber"
 )
 
 cold_complex = cold_system.add_xtal_water( 
-    file=f"{project_dir}/inputs/hybrid_model/protein/wat_h.pdb",
+    file=f"{project_dir}/inputs/hybrid_model/protein/{system_name}/wat_h.pdb",
 )
 
 parameterised_ligand = cold_complex.ligand.parameterise(
-    path=f"{project_dir}/inputs/hybrid_model/ligands/{ligand_name}/",
+    path=f"{project_dir}/inputs/hybrid_model/ligands/{system_name}/{ligand_name}/",
 )
 
 parameterised_hydroxide = cold_complex.non_standard_residue.parameterise(
-    path=f"{project_dir}/inputs/hybrid_model/ligands/{ligand_name}",
+    path=f"{project_dir}/inputs/hybrid_model/ligands/{system_name}/{ligand_name}",
     atom_type="amber",
     residue_name="MOH"
 )
 
 cold_complex.prepare_metals_for_ezaff(
-<<<<<<< HEAD
-    path=f"{project_dir}/inputs/hybrid_model/ligands/{ligand_name}/"
+    path=f"{project_dir}/inputs/hybrid_model/ligands/{system_name}/{ligand_name}/"
 )
 
 prepared_complex = cold_complex.write_complex(
-    path=f"{project_dir}/inputs/hybrid_model/ligands/{ligand_name}/",
-=======
-    path=f"{project_dir}/inputs/ligands/{ligand_name}/"
-)
-
-prepared_complex = cold_complex.write_complex(
-    path=f"{project_dir}/inputs/ligands/{ligand_name}/",
->>>>>>> 3049430 (wip: prepare ezaff/hybrid model)
+    path=f"{project_dir}/inputs/hybrid_model/ligands/{system_name}/{ligand_name}/",
     ligand_name=ligand_name
 )
 
