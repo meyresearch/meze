32a0e09b836b2281e982ae40a2c1be2313f01e27
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2026-01-15 14:56:42 +0000

    feat: update slurm script

--- examples/solvate.sh
+++ examples/solvate.sh
@@ -2,6 +2,7 @@
 #SBATCH --nodes=1
 
 project_dir=$1
+system_name=$2
 lig_i=$SLURM_ARRAY_TASK_ID
 
-python solvate.py $project_dir "ligand_${lig_i}"
+python solvate.py $project_dir $system_name "ligand_${lig_i}"
--- examples/submit_heat.sh
+++ examples/submit_heat.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+mapfile -t files < ligands.txt   
+
+numbers=()
+for f in "${files[@]}"; do
+  numbers+=( "${f#ligand_}" )  
+done
+
+#echo "${numbers[@]}"
+
+arr_str=$(IFS=,; echo "${numbers[*]}")
+
+module load apps/amber
+
+LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
+
 project_dir=$1
+system_name=$2
 
 for i in 1 2 3
 do
 	sbatch --error=../logs/heat_%x_%a.err \
        	       --output=../logs/heat_%x_%a.out \
-       	       --array=1-16 \ #FIXEM
-       	       heat.sh $project_dir $i
+       	       --array="$arr_str" \ 
+       	       heat.sh $project_dir $system_name $i
 done
