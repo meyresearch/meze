bd555d78c0f95c7b2f7f215a2e4cd06bf9dfc768
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2026-01-15 14:59:34 +0000

    fix: add solvation script

--- examples/submit_solvation.sh
+++ examples/submit_solvation.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-project_dir=$1
+mapfile -t files < ligands.txt   
 
-sbatch --error=../logs/solv_%x_%a.err \
-       --output=../logs/solv_%x_%a.out \
-       --array=1-16 \ #FIXME
-       solvate.sh $project_dir 
+numbers=()
+for f in "${files[@]}"; do
+  numbers+=( "${f#ligand_}" )  
+done
+
+#echo "${numbers[@]}"
+
+arr_str=$(IFS=,; echo "${numbers[*]}")
+
+module load apps/amber
+
+LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
+
+project_dir=$1
+system_name=$2
+
+for i in 1 2 3
+do
+	sbatch --error=../logs/solvate_%x_%a.err \
+       	       --output=../logs/solvate_%x_%a.out \
+       	       --array="$arr_str" \ 
+       	       solvate.sh $project_dir $system_name $i
+done
