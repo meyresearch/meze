054b08e9900789fe7c692dba89781dc6f1670789
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2026-03-10 16:38:21 +0000

    feat: loop over entries in sofra

--- examples/hybrid_model/vim2/average_hybrid_model_charges.py
+++ examples/hybrid_model/vim2/average_hybrid_model_charges.py
@@ -3,18 +3,23 @@ import sys
 import json
 
 project_dir = "/Users/af25016/projects/meze/data/" 
+project_dir = "/Users/af25016/projects/hybrid_model/"
 system_name = "vim2"
-ligand_name = "ligand_11"
+
+
+# replace with reading in all ligands in the sofra!!
 # ligands 
 
 with open(f"{project_dir}/inputs/hybrid_model/protein/{system_name}/model_ezaff_sofra.json", "r") as file:
      json_recipe = json.load(file)
 
-parameterisation_directory = json_recipe["ligand_11"]["parameterisation_directory"]
+for i, ligand in enumerate(json_recipe.keys()):
+    ligand_name = ligand
+    parameterisation_directory = json_recipe[ligand_name]["parameterisation_directory"]
 
 
-prepared_meze = ColdMeze.load(
-    filename=f"{parameterisation_directory}/{ligand_name}_fixed_charges_solvated.pkl"
-)
+    prepared_meze = ColdMeze.load(
+        filename=f"{parameterisation_directory}/{ligand_name}_fixed_charges_solvated.pkl"
+    )
 
 
