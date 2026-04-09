a54ccf51b76982cc863d57eb214bd8920b55e631
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2026-04-09 11:33:50 +0100

    wip: rbfe prep with model 0 using sofra class

--- examples/model_0/vim2/prepare_rbfe.py
+++ examples/model_0/vim2/prepare_rbfe.py
@@ -16,11 +16,7 @@ project_dir = DATA_DIR
 #DEBUGGING
 project_dir = "/Users/af25016/projects/vim2-model-0/"
 
-try: #DEBUGGING; for normal running, remove try/except block
-     model_0_sofra = Sofra.from_file(f"{project_dir}/inputs/model_0/protein/{system_name}/model_0_sofra.json")
-except RuntimeError as e:
-     print(e)
-     print("Constructing Sofra manually")
+model_0_sofra = Sofra.from_file(f"{project_dir}/inputs/model_0/protein/{system_name}/model_0_sofra.json")
 
 with open(f"{project_dir}/inputs/model_0/protein/{system_name}/model_0_sofra.json", "r") as file:
      sofra_file = json.load(file)
@@ -34,33 +30,6 @@ json_recipe["path_to_engine"] = os.path.join(
 
 ligand_path = f"{project_dir}/inputs/model_0/ligands/{system_name}/"
 
-mezes = []
-for i, ligand_name in enumerate(sofra_file.keys()):
-     input_dir = f"{project_dir}/inputs/model_0/protein/{system_name}/solvate_{ligand_name}_bound"
-
-     solvated_meze = ColdMeze.from_files(
-          topology=f"{input_dir}/{ligand_name}_complex_solv.prmtop",
-          coordinates=f"{input_dir}/{ligand_name}_complex_solv.inpcrd",
-          recipe=ColdMezeRecipe(**json_recipe)
-     )
-     mezes.append(solvated_meze)
-     pickle_file = solvated_meze.save(
-          filename=f"{input_dir}/{ligand_name}_solvated.pkl"
-     )
-
-     solvated_meze.add_to_sofra(
-     f"{project_dir}/inputs/model_0/protein/{system_name}/model_0_sofra.json",
-     key=ligand_name,
-     pickle_file=pickle_file
-     )
-try: #DEBUGGING; for normal running, remove try/except block
-     model_0_sofra = Sofra.from_file(f"{project_dir}/inputs/model_0/protein/{system_name}/model_0_sofra.json")
-except RuntimeError as e:
-     print(e)
-     print("Constructing Sofra manually")
-
-
-    
 ligand_files = sorted(glob.glob(f"{ligand_path}/ligand_*/ligand_*.pdb"))
 
 for i, ligand_file in enumerate(ligand_files):
