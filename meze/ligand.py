ac33d149a6a5b59ae70bbb3947471a00ee1edecf
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2026-04-15 17:58:51 +0100

    feat: get ligand resname and set top and coords attributes for ligand
    rebase: fix unbound equil

--- meze/ligand.py
+++ meze/ligand.py
@@ -329,9 +329,13 @@ class Ligand():
                 solvated_coordinates
             )
             system = bss.IO.readMolecules([solvated_topology, solvated_coordinates])
+
             solvated_ligand = dataclasses.replace(
                 ligand,
-                file=[solvated_topology, solvated_coordinates],
+                file=ligand_mol2,
+                frcmod_file=ligand_frcmod,
+                topology=solvated_topology,
+                coordinates=solvated_coordinates,
                 system=system,
                 parameterised=True
             )
