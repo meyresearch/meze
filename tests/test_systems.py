765cf5a1344edd88c34808d601fd579c5252fad7
jasmin.guven@hotmail.com <jasmin.guven@hotmail.com>
2026-08-11 17:36:11 +0100

    tests: test additional rests atom group raises

--- tests/test_systems.py
+++ tests/test_systems.py
@@ -221,6 +221,15 @@ def test_build_distance_restraints_wrong_type_raises(vim2_cold_meze):
         )
 
 
+def test_build_distance_restraints_wrong_value_type_raises(vim2_cold_meze):
+    with pytest.raises(
+        TypeError, match="coordinating_residues values must be "
+    ):
+        vim2_cold_meze.build_distance_restraints(
+            coordinating_residues={3450: "some string"}
+        )
+
+
 def test_ligand_not_in_distance_restraints(vim2_top_and_coord):
     restraints = vim2_top_and_coord.build_distance_restraints()
     for (_, ligand_id) in restraints.keys():
